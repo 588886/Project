df90cc2fe4d0fd023884b0ef8dadbffc87bd11f8
tkd5659 <tkd5659@naver.com>
2023-11-30 12:48:01 +0900

    첫번째 커밋
    첫번째 커밋 공지사항 화면연결

---
+++ smartfarm/src/main/java/com/hanul/smartfarm/notice/NoticeVO.java
@@ -0,0 +1,14 @@
+package com.hanul.smartfarm.notice;
+
+import java.sql.Date;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class NoticeVO {
+
+	private int id, readcnt, no, rid, root, step, indent;
+	private String title, content, writer, name, filepath, filename;
+	private Date writedate;
+}
